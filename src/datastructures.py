3152808e63774e9d2f6ea8c14c7df27eeadcc0a8
Rebillot <125141214+Rebillot@users.noreply.github.com>
2023-06-06 01:52:40 +0000

    working on in, first commit

--- src/datastructures.py
+++ src/datastructures.py
@@ -21,15 +21,26 @@ class FamilyStructure:
 
     def add_member(self, member):
         # fill this method and update the return
-        pass
+        new_member = member
+        if 'id' not in new_member:
+            new_member['id']= self._generateId()
+        self._members.append(new_member)  
+        return new_member  
 
     def delete_member(self, id):
         # fill this method and update the return
-        pass
+        for member in self._members:
+            if member['id']== id:
+                self._members.remove(member)
+            return {'Member deleted': True}
+        return {'error': 'Member not found'}
 
     def get_member(self, id):
         # fill this method and update the return
-        pass
+        for member in self._members:
+            if member['id']== id:
+                return member
+        return {'error': 'Member not found'}
 
     # this method is done, it returns a list with all the family members
     def get_all_members(self):
